5733072a04e331cd7184f020e350fcaa7a917d85
Nathaniel Martinez <nathanielmartinez2001@gmail.com>
2021-02-14 21:02:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,6 +8,9 @@ final_results = data.each_with_object({}) do |(key, value), final_array|
         final_array[name] = {}
       end
       if !final_array[name][key] = []
+        !final_array[name][key] = []
+    end
+        
     end
     final_array[name][key].push(inner_key.to_s)
   end
