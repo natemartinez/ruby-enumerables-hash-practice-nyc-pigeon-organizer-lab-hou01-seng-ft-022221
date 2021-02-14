36e565090e721de000740ead861391735fe4d1f6
Nathaniel Martinez <nathanielmartinez2001@gmail.com>
2021-02-14 20:41:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ def nyc_pigeon_organizer(data)
 data.each_with_object({}) do |(key, value), final_array|
   value.each do |inner_key, names|
     
-    name.each do 
+    name.each do |name|
 final_array
 end
 end
